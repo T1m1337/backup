5c4d83168aa56763673e24c94d8635daee47bb76
T1m1337 <32897497+T1m1337@users.noreply.github.com>
2017-10-18 11:42:26 +0200

    Add files via upload

---
+++ Backup/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.Location = new System.Drawing.Point(16, 31);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.checkBox1.Size = new System.Drawing.Size(66, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Desktop";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(16, 55);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Documents";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(16, 79);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(64, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Pictures";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(158, 244);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Backup!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(21, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Select all";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(288, 244);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp2.Properties.Resources.HSR_Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(221, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(166, 84);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 217);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(232, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Zielpfad:";
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(16, 103);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(91, 17);
+            this.checkBox4.TabIndex = 10;
+            this.checkBox4.Text = "Anderer Pfad:";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 127);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(228, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(250, 125);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(29, 22);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 296);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Tim\'s Backup";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Backup/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private object openFileDialog1;
+        private object form1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            checkBox1.Checked = true;
+            checkBox2.Checked = true;
+            checkBox3.Checked = true;
+            checkBox4.Checked = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders"; //Registry Key wo Standardpfade angegeben sind
+            RegistryKey registry = Registry.CurrentUser.OpenSubKey(path); //öffnet den Unterordner des Keys
+            if (checkBox1.Checked == true)
+            {
+                string name = registry.GetValue("Desktop").ToString(); // wandelt den Pfad aus dem Key in einen String um
+                string var = Environment.ExpandEnvironmentVariables(name); // wenn eine Environment Variable vorhanden ist, wird sie ausgeklappt
+                string target = textBox1.Text; // liest Text aus Textfeld aus
+                string desktop = "\\Desktop";
+                Directory.CreateDirectory(target); // erstellt Ordner im Zielverzeichnis
+                string sourcefile = name; //eigentlich sourcedirectory
+                string targetpath = target + desktop;
+                foreach (string dirPath in Directory.GetDirectories(sourcefile, "*",
+                SearchOption.AllDirectories))
+                {
+                        Directory.CreateDirectory(dirPath.Replace(sourcefile, targetpath));
+                }
+                foreach (string newPath in Directory.GetFiles(sourcefile, "*.*", SearchOption.AllDirectories)) // Diese Schleifen kopieren die Files und überschreiben die alten.
+                {
+                    File.Copy(newPath, newPath.Replace(sourcefile, targetpath), true);
+                }
+            }
+
+            if (checkBox2.Checked == true)
+            {
+                string name = registry.GetValue("Personal").ToString();
+                string var = Environment.ExpandEnvironmentVariables(name);
+                string target = textBox1.Text;
+                string documents = "//Documents";
+                Directory.CreateDirectory(target);
+                string sourcefile1 = var;
+                string targetpath1 = target + documents;
+                foreach (string dirPath in Directory.GetDirectories(sourcefile1, "*",
+                SearchOption.AllDirectories))
+                {
+                    Directory.CreateDirectory(dirPath.Replace(sourcefile1, targetpath1));
+                }
+                foreach (string newPath in Directory.GetFiles(sourcefile1, "*.*", SearchOption.AllDirectories))
+                {
+                    File.Copy(newPath, newPath.Replace(sourcefile1, targetpath1), true);
+                }
+            }
+            
+
+            if (checkBox3.Checked == true)
+            {
+                string name = registry.GetValue("My Pictures").ToString();
+                string var = Environment.ExpandEnvironmentVariables(name);
+                string target = textBox1.Text;
+                string pictures = "//Pictures";
+                Directory.CreateDirectory(target);
+                string sourcefile2 =  var;
+                string targetpath2 = target + pictures;
+
+                foreach (string dirPath in Directory.GetDirectories(sourcefile2, "*",
+                SearchOption.AllDirectories))
+                   Directory.CreateDirectory(dirPath.Replace(sourcefile2, targetpath2));
+
+                foreach (string newPath in Directory.GetFiles(sourcefile2, "*.*", SearchOption.AllDirectories))
+                {
+                    File.Copy(newPath, newPath.Replace(sourcefile2, targetpath2), true);
+                }
+            }
+
+
+            if (checkBox4.Checked == true)
+            {
+                string target = textBox1.Text;
+                Directory.CreateDirectory(target);
+                string data = "//data";
+                string targetpath3 = target + data;
+                string sourcefile3 = textBox2.Text;
+
+                foreach (string dirPath in Directory.GetDirectories(sourcefile3, "*", SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace(sourcefile3, targetpath3));
+
+                foreach (string newPath in Directory.GetFiles(sourcefile3, "*.*", SearchOption.AllDirectories))
+                {   
+                    File.Copy(newPath, newPath.Replace(sourcefile3, targetpath3), true);
+                }
+            }
+          
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.hsr.ch/de/");
+        }
+
+        private void checkBox4_Click(object sender, EventArgs e)
+        {
+            if (checkBox4.CheckState != CheckState.Checked)
+            {
+                textBox2.Enabled = false;
+            }
+            else
+            {
+                textBox2.Enabled = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string path = null;
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    path = fbd.SelectedPath;
+                }
+            }
+            textBox2.Text = path;
+        }
+    }
+}
